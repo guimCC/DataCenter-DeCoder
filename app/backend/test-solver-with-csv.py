81266ab061b3cb0d9f0af701bd5ac5b7fbac26c1
Gerard <gerardgraugarcia@gmail.com>
2025-05-04 02:01:03 +0200

    update custom imports

--- app/backend/test-solver-with-csv.py
+++ app/backend/test-solver-with-csv.py
@@ -1,7 +1,7 @@
 import csv
 import os
 from collections import defaultdict
-from solver_utils import solve_module_list
+from app.backend.solver_utils_list import solve_module_list
 from models import Module, IOField # Changed import source
 
 # --- Define file paths relative to this script ---
