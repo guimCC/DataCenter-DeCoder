e2c83abde8efafcc083fdaaf13728890e3a76ac1
Gerard <gerardgraugarcia@gmail.com>
2025-05-04 08:07:49 +0200

    remove id's greaters than 19

--- app/backend/mongo_utils.py
+++ app/backend/mongo_utils.py
@@ -1,3 +1,4 @@
+from numpy import delete
 import pymongo
 
 # Conexión hardcodeada a MongoDB Atlas
@@ -31,6 +32,23 @@ def get_all_modules():
     collection = db.modules
     return list(collection.find({}, {"_id": 0}))
 
+def delete_modules_by_id_greater_than(threshold_id):
+    """Deletes modules from the database where ID is greater than the threshold."""
+    try:
+        db = get_database()
+        collection = db.modules
+        # Assuming the field name for the ID is 'ID'
+        query = {"id": {"$gt": threshold_id}}
+        result = collection.delete_many(query)
+        return result.deleted_count
+    except Exception as e:
+        return 0
+
+
 # Para probar la conexión cuando se ejecuta este archivo directamente
 if __name__ == "__main__":
     test_connection()
+    # delete_modules_by_id_greater_than(19)
+    all_modules = get_all_modules()
+    ids = {module['id'] for module in all_modules if 'id' in module}
+    print(ids)
